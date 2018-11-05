d91653108dffd50eba287870947a4f6b04a916fd
x00158551 <44375318+x00158551@users.noreply.github.com>
2018-11-05 20:00:19 +0000

    Add files via upload

---
+++ Account.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bank
+{
+    class Account
+    {
+        private double balance;                             // the account balance
+        private double overdraftLimit;
+
+        // construct a bank acocunt with specified opening
+        // balance and overdraft limit
+        public Account(double balance, double overdraftLimit)
+        {
+            if (balance >= 0)
+            {
+                this.balance = balance;
+            }
+            else
+            {
+                throw new ArgumentException("overdraft limit must be >= 0");
+            }
+            this.OverdraftLimit = overdraftLimit;
+        }
+
+        // chain, 0 balance and overdraft
+        public Account() : this(0, 0)
+        {
+
+        }
+
+        // read-only property
+        public double Balance
+        {
+            get
+            {
+                return balance;
+            }
+        }
+
+        // property
+        public double OverdraftLimit
+        {
+            get
+            {
+                return overdraftLimit;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    this.overdraftLimit = value;
+                }
+                else
+                {
+                    throw new ArgumentException("overdraft limit must be >= 0");
+                }
+            }
+        }
+
+        // deposit some money
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                balance += amount;
+            }
+            else
+            {
+                throw new ArgumentException("amount must be > 0");
+            }
+        }
+
+        // withdraw some money if sufficient funds
+        public void Withdraw(double amount)
+        {
+            if (amount > 0)
+            {
+                if (balance + overdraftLimit >= amount)
+                {
+                    balance -= amount;
+                }
+                else
+                {
+                    throw new ArgumentException("Insufficent funds for this transaction");
+                }
+            }
+            else
+            {
+                throw new ArgumentException("amount must be > 0");
+            }
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Bank Account Details:\nBalance: {0:00.00}, OverDraft: {1:00.00} ",
+                               balance, overdraftLimit);
+        }
+
+    }
+}
---
+++ MainBankProgram.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bank
+{
+    class MainBankProgram
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("******                     *******");
+            Console.WriteLine("****** Bank functionality  *******");
+
+            Account myBankAcc = new Account(500, 500);
+            myBankAcc.Deposit(500);
+            Console.WriteLine(myBankAcc);
+            myBankAcc.Withdraw(1000);
+            Console.WriteLine(myBankAcc);
+            myBankAcc.Withdraw(500);
+            Console.WriteLine(myBankAcc);
+            Console.Read();
+        }
+    }
+}
